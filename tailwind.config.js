f0e8bbb1c6db1c0e3fa2fef150aa025293ad2af9
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-12-01 20:56:21 +0700

    setup font family & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
+        sourceSans: ["Source Sans 3", "sans-serif"],
+      },
+      colors: {
+        primary: "#101C45",
+        secondary: "rgba(255, 255, 255, 0.80)",
+        info: "#9BEAE0",
+      },
+    },
   },
   plugins: [],
-}
+};
