29140f3f79e064f36b50aa0f7f057d8ede2455b5
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-12-02 10:42:06 +0700

    complete navbar section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,50 @@
+import { holadokLogo } from "../assets";
+
 const Nav = () => {
   return (
     <header className="padding-x padding-top w-full">
       <nav className="flex justify-between items-center max-w-screen-2xl">
-        <a href="#" className="text-primary font-source-sans font-bold text-lg">
-          <img src="../assets/holadok-logo.png" alt="Logo" />
-          <span>Holadok</span>
-        </a>
+        <div className="flex flex-column justify-center items-center gap-2">
+          <img src={holadokLogo} width={30} height={30} alt="Logo" />
+          <a
+            href="#"
+            className="text-primary font-source-sans font-bold text-lg"
+          >
+            Holadok
+          </a>
+        </div>
         <ul className="flex justify-center items-center gap-10">
           <li>
-            <a href="#">Home</a>
+            <a
+              href="#"
+              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 nav-active"
+            >
+              Home
+            </a>
           </li>
           <li>
-            <a href="#">Features</a>
+            <a
+              href="#"
+              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
+            >
+              Features
+            </a>
           </li>
           <li>
-            <a href="#">Blog</a>
+            <a
+              href="#"
+              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
+            >
+              Blog
+            </a>
           </li>
           <li>
-            <a href="#">About Us</a>
+            <a
+              href="#"
+              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
+            >
+              About Us
+            </a>
           </li>
         </ul>
         <div className="flex gap-4">
