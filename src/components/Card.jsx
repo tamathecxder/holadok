3f5c6949eec4dfa7bad8f988a75491e9845ea701
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-12-02 19:35:19 +0700

    change card components to make them responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
     "https://images.squarespace-cdn.com/content/v1/50204bd5e4b03f6f4d185490/1675116974621-JREYH3AD3EW7V3OIND7N/Actor-Headshots-Markham-Headshots-0013%2B1.jpg";
 
   return (
-    <div className="max-w-[300px] border border-slate-100 p-5 shadow-xl bg-white absolute top-40 right-[19rem] rounded-xl">
+    <div className="xl:block hidden w-full sm:max-w-[300px] border border-slate-100 p-5 shadow-xl bg-white static max-sm:mb-10 lg:absolute top-40 right-[19rem] rounded-xl">
       <div className="flex justify-between items-center mb-3">
         <div className="flex flex-row justify-start items-center gap-4">
           <div className="w-8 h-8 relative rounded-full overflow-hidden">
@@ -18,7 +18,7 @@ const Card = () => {
           <img src={quote} alt="Quote Icon" className="object-cover w-2" />
         </div>
       </div>
-      <p className="text-xs font-sourceSans font-normal text-primary leading-normal">
+      <p className="sm:text-xs text-md font-sourceSans font-normal text-primary leading-normal">
         “The doctor appointment app has been great for me. Scheduling
         appointments and getting medication reminders is much easier, and I love
         the option for virtual consultations. I'd recommend this app to others.”
