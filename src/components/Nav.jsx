613bd4209f29c62123e5d8f2614a95d2a2f5f9b9
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-12-02 18:57:26 +0700

    refactoring navbar component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,51 +2,53 @@ import { holadokLogo } from "../assets";
 
 const Nav = () => {
   return (
-    <header className="padding-x padding-top w-full">
-      <nav className="flex justify-between items-center max-w-screen-2xl">
-        <div className="flex flex-column justify-center items-center gap-2">
-          <img src={holadokLogo} width={30} height={30} alt="Logo" />
-          <a
-            href="#"
-            className="text-primary font-source-sans font-bold text-lg"
-          >
-            Holadok
-          </a>
+    <header className="padding-x padding-top w-full absolute z-10">
+      <nav className="flex justify-between items-center max-container">
+        <div className="flex gap-[90px]">
+          <div className="flex flex-column justify-center items-center gap-2">
+            <img src={holadokLogo} width={30} height={30} alt="Logo" />
+            <a
+              href="#"
+              className="text-primary font-source-sans font-bold text-lg"
+            >
+              Holadok
+            </a>
+          </div>
+          <ul className="flex justify-center items-center gap-10">
+            <li>
+              <a
+                href="#"
+                className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 nav-active"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
+              >
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
+              >
+                About Us
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="flex justify-center items-center gap-10">
-          <li>
-            <a
-              href="#"
-              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 nav-active"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
-            >
-              Features
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
-            >
-              Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="font-sourceSans text-base font-normal transition-all hover:text-slate-400 text-slate-800"
-            >
-              About Us
-            </a>
-          </li>
-        </ul>
         <div className="flex gap-4">
           <button className="btn-cta">Login</button>
           <button className="border border-primary btn-cta">Register</button>
