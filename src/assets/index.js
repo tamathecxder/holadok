1e5ab857e1650acfd3563b5cfbdb17962b7188af
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-12-02 10:40:38 +0700

    importing assets into index file

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import calendar from "./calendar.png";
+import docModel from "./doc-model.png";
+import holadokLogo from "./holadok-logo.png";
+import notification from "./notification.png";
+import quote from "./quote.png";
+import target from "./target.png";
+
+export { calendar, docModel, holadokLogo, notification, quote, target };
